b172564f4d116293a3b463337058b461390eb111
Lawrence Law <d201.c317@gmail.com>
2017-03-01 09:48:16 +0000

    Tidy

--- schema.js
+++ schema.js
@@ -1,16 +1,17 @@
 "use strict";
 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 exports.schema = Schema({
-    facility: Number,
-    severity: Number,
-    tag: { type: [String], index: true },
-    time: Date,
-    hostname: { type: [String], index: true },
-    address: String,
-    family: String,
-    port: Number,
-    size: Number,
-    msg: String
+  facility: Number,
+  severity: Number,
+  tag: { type: [String], index: true },
+  time: Date,
+  hostname: { type: [String], index: true },
+  address: String,
+  family: String,
+  port: Number,
+  size: Number,
+  msg: String
 }, { capped: 2048 });
