c483a457cdc6cda7bb16e30800b0efbd957eac7c
Lawrence Law <d201.c317@gmail.com>
2017-03-01 09:40:28 +0000

    Code Style

--- index.js
+++ index.js
@@ -4,27 +4,29 @@
 
 const mongoose = require("mongoose");
 const Syslogd = require("syslogd");
+const schema = require("./schema.js");
 
 class App {
-    constructor(mongodb_uri) {
-        mongoose.connect(mongodb_uri);
-        const db = mongoose.connection;
-        db.on("error", console.error.bind(console, 'MongoDB Connection Error: '));
-        db.once("open", () => {
-            console.log("MongoDB Connection Established.");
-        });
-        this.Syslog = db.model('Syslog', require('./schema.js').schema);
-    }
+  constructor(mongodb_uri) {
+    mongoose.connect(mongodb_uri);
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB Connection Error: "));
+    db.once("open", () => {
+      console.log("MongoDB Connection Established.");
+    });
+    this.Syslog = db.model("Syslog", schema.schema);
+  }
 
-    listen() {
-        Syslogd(function(info) {
-          this.Syslog.create(info, (err, log) => {
-            if (err) return console.error.bind(console, err);
-          });
-        }).listen(514, function(err) {
-            console.log('start')
-        });
-    }
+  listen() {
+    Syslogd(function(info) {
+      this.Syslog.create(info, (err) => {
+        if (err) return console.error.bind(console, err);
+      });
+    }).listen(514, function(err) {
+      console.log("start");
+      if (err) return console.error.bind(console, err);
+    });
+  }
 }
 
 const app = new App(process.env.MONGODB_URI);
