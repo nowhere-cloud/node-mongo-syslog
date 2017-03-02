5eb8f14f6cb7c820281ef2b715837f3d3ac69095
Lawrence Law <d201.c317@gmail.com>
2017-03-02 14:29:09 +0000

    Code Annotations

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const mongoose = require("mongoose");
 const Syslogd = require("syslogd");
 const schema = require("./schema.js");
 
+/**
+ * Application Core
+ */
 class App {
+  /**
+   * Defining how the Syslog Storage Works.
+   * All data will goes to a Collection called as 'syslog'.
+   * Absolutely, these code are from the Quickstart of Mongoose.
+   * @param mongodb_uri mongodb://server/database
+   */
   constructor(mongodb_uri) {
     mongoose.connect(mongodb_uri);
     const db = mongoose.connection;
@@ -18,13 +27,23 @@ class App {
   }
 
   listen() {
+    /* This const is used for repelling problem of `this` scoping */
     const model = this.Model;
+    /**
+     *  This is where all the syslog logic goes.
+     *  The Syslog parser will pare the incoming log into a specified dataset
+     *  Format documented at 'schema.js', and exposed to `info`
+     */
     Syslogd(function(info) {
+      /* Since the dataset format is good enough. It will pumped into MongoDB Directly. */
       model.create(info, (err) => {
+        /* Error Handling. */
         if (err) return console.error.bind(console, err);
       });
     }).listen(514, function(err) {
+      /* Indicating the Syslog Collector has been started correctly */
       console.log("start");
+      /* Also, Error Handling. */
       if (err) return console.error.bind(console, err);
     });
   }
--- schema.js
+++ schema.js
@@ -2,7 +2,10 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+/**
+ * This is the Syslog parser parsed format.
+ * @type Hash
+ */
 exports.schema = Schema({
   facility: Number,
   severity: Number,
