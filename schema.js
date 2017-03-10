b615bf536f371137abb4a337d59d480f96103f15
Lawrence Law <d201.c317@gmail.com>
2017-03-10 17:32:05 +0000

    New Standard

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 /**
  * This is the Syslog parser parsed format.
@@ -11,28 +11,28 @@ const Schema = mongoose.Schema;
  * https://stackoverflow.com/questions/37347802/find-by-id-with-mongoose
  */
 const syslogSchemas = new Schema({
-    facility: Number,
-    severity: Number,
-    tag: {
-        type: String,
-        index: true
-    },
-    time: Date,
-    hostname: {
-        type: String,
-        index: true
-    },
-    address: String,
-    family: String,
-    port: Number,
-    size: Number,
-    msg: String
+  facility: Number,
+  severity: Number,
+  tag: {
+    type: String,
+    index: true
+  },
+  time: Date,
+  hostname: {
+    type: String,
+    index: true
+  },
+  address: String,
+  family: String,
+  port: Number,
+  size: Number,
+  msg: String
 }, {
-    capped: {
-        size: 5242880,
-        max: 4096,
-        autoIndexId: true
-    }
+  capped: {
+    size: 5242880,
+    max: 4096,
+    autoIndexId: true
+  }
 });
 
-module.exports = mongoose.model("Syslog", syslogSchemas);
+module.exports = mongoose.model('Syslog', syslogSchemas);
--- syslogd.js
+++ syslogd.js
@@ -1,49 +1,49 @@
 #!/usr/bin/node
 
-"use strict";
+'use strict';
 
-const mongoose = require("mongoose");
-const Syslogd = require("syslogd");
-const Syslog = require("./schema.js");
+const mongoose = require('mongoose');
+const Syslogd = require('syslogd');
+const Syslog = require('./schema.js');
 
 /**
  * Application Core
  */
 class App {
-    /**
-     * Defining how the Syslog Storage Works.
-     * All data will goes to a Collection called as 'syslog'.
-     * Absolutely, these code are from the Quickstart of Mongoose.
-     * @param mongodb_uri mongodb://server/database
-     */
-    constructor(mongodb_uri) {
-        mongoose.connect(mongodb_uri);
-        const db = mongoose.connection;
-        db.on("error", console.error.bind(console, "MongoDB Connection Error: "));
-        db.once("open", () => {
-            console.log("MongoDB Connection Established.");
-        });
-    }
+  /**
+   * Defining how the Syslog Storage Works.
+   * All data will goes to a Collection called as 'syslog'.
+   * Absolutely, these code are from the Quickstart of Mongoose.
+   * @param mongodb_uri mongodb://server/database
+   */
+  constructor(mongodb_uri) {
+    mongoose.connect(mongodb_uri);
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB Connection Error: '));
+    db.once('open', () => {
+      console.log('MongoDB Connection Established.');
+    });
+  }
 
-    listen() {
-        /**
-         *  This is where all the syslog logic goes.
-         *  The Syslog parser will pare the incoming log into a specified dataset
-         *  Format documented at 'schema.js', and exposed to `info`
-         */
-        Syslogd(function(info) {
-            var log = new Syslog(info);
-            /* Since the dataset format is good enough. It will pumped into MongoDB Directly. */
-            log.save((err) => {
-                if (err) throw err;
-            });
-        }).listen(514, function(err) {
-            /* Indicating the Syslog Collector has been started correctly */
-            console.log("start");
-            /* Also, Error Handling. */
-            if (err) return console.error.bind(console, err);
-        });
-    }
+  listen() {
+    /**
+     *  This is where all the syslog logic goes.
+     *  The Syslog parser will pare the incoming log into a specified dataset
+     *  Format documented at 'schema.js', and exposed to `info`
+     */
+    Syslogd(function(info) {
+      var log = new Syslog(info);
+      /* Since the dataset format is good enough. It will pumped into MongoDB Directly. */
+      log.save((err) => {
+        if (err) throw err;
+      });
+    }).listen(514, function(err) {
+      /* Indicating the Syslog Collector has been started correctly */
+      console.log('start');
+      /* Also, Error Handling. */
+      if (err) return console.error.bind(console, err);
+    });
+  }
 }
 
 const app = new App(process.env.MONGODB_URI);
