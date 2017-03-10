32bfccbb5dfab1a1a85370975b6bf6ef96f9ce1d
Lawrence Law <d201.c317@gmail.com>
2017-03-10 06:39:00 +0000

    Upgraded to 4096

--- schema.js
+++ schema.js
@@ -6,6 +6,9 @@ const Schema = mongoose.Schema;
  * This is the Syslog parser parsed format.
  * @type Hash
  * http://stackoverflow.com/questions/33791714/data-type-to-store-time-with-mongoose
+ *
+ * _id:
+ * https://stackoverflow.com/questions/37347802/find-by-id-with-mongoose
  */
 const syslogSchemas = new Schema({
     facility: Number,
@@ -27,7 +30,7 @@ const syslogSchemas = new Schema({
 }, {
     capped: {
         size: 5242880,
-        max: 2048,
+        max: 4096,
         autoIndexId: true
     }
 });
